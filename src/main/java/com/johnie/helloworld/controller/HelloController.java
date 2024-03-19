9c8615395643d78a970689a2139befcdf7384fa4
johniecheng <johniecheng@gmail.com>
2024-03-19 21:28:12 +0800

    finished

---
+++ src/main/java/com/johnie/helloworld/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.johnie.helloworld.controller;/**
+ *  <p>TODO 这里用一句话描述这个类的作用. </p>
+ *  <p>TODO类使用说明</p>
+ *
+ * @author  johniecheng
+ * @version 1.0
+ * @date    2024/3/19 21:21
+ */public class HelloController {
+}
