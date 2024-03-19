1505ad29d7dd621ff093488675fe57b280836347
johniecheng <johniecheng@gmail.com>
2024-03-19 21:30:05 +0800

    finished

--- src/main/java/com/johnie/helloworld/controller/HelloController.java
+++ src/main/java/com/johnie/helloworld/controller/HelloController.java
@@ -1,9 +1,21 @@
-package com.johnie.helloworld.controller;/**
- *  <p>TODO 这里用一句话描述这个类的作用. </p>
- *  <p>TODO类使用说明</p>
+package com.johnie.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  *
- * @author  johniecheng
+ * @author johniecheng
  * @version 1.0
- * @date    2024/3/19 21:21
- */public class HelloController {
+ * @date 2024/3/19 21:21
+ */
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello world!";
+    }
 }
